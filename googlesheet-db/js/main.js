319d877fb68ee6cf1d979384da5b8e62106cc7df
Laura Crothers <lrcrothers@gmail.com>
2020-09-08 14:17:46 -0700

    Update main.js

--- googlesheet-db/js/main.js
+++ googlesheet-db/js/main.js
@@ -221,7 +221,7 @@ function createDataTable() {
     MyApp.oTable = $("#spreadsheet").dataTable({
         "aoColumnDefs": [
             //{ "sType": "link-content", "aTargets": [ 0 ] },
-            { "asSorting": [ "desc" ], "aTargets": [ 7 ] },
+            { "asSorting": [ "desc" ], "aTargets": [ 2 ] },
             { "bVisible": false, "aTargets": [ -2, -3, -1 ] } //hide the keywords column for now (the last column, hence -1)
         ],
         "iDisplayLength": 20,
