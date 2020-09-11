676a2e36e7eebb11a19061ffdfa7279eaae1746e
Laura Crothers <lrcrothers@gmail.com>
2020-09-11 10:28:34 -0700

    dsf
    dsf

---
+++ datatable-db/js/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+    var results = [];
+    var url = "https://spreadsheets.google.com/feeds/list/1CS6TU1metcFNxhuWKDg7TmyPWdlffp7t-HTZbYdG5Ck/1/public/values?alt=json";
+    
+    var jqxhr = $.getJSON(url, function() {
+        console.log( "success" );
+	})
+  	.done(function(data) {
+    	console.log( "second success" );
+        var entry = data.feed.entry;
+ 		$(entry).each(function(){
+            results.push([this.gsx$title.$t , this.gsx$year.$t, this.gsx$categories.$t, this.gsx$type.$t, this.gsx$link.$t]);
+  		});
+	    var table = $('#example').DataTable( {
+    		data: results,
+        dom: 'Pfrtip',
+        searchPanes:{
+          controls: false,
+          cascadePanes: true,
+      threshold: 1,
+      columns: [2,3]
+    }, 
+           "order": [ 1, 'desc' ],
+        columns: [
+            { title: "Title" },
+            { title: "Year" },
+            { title: "Topic Areas"},
+            { title: "Material Type" },
+						{ title: "Link",
+                "render": function ( data, type, row, meta ) {
+      return '<a href="'+data+'">View File</a>';
+            },
+             
+        }],
+    	columnDefs: [
+      {
+        targets: [2],
+        "visible": false,
+        render: function (data, type, row) {
+          if (type === 'sp') {
+            return data.split('; ')
+          }
+          return data;
+        },
+        searchPanes: {
+          orthogonal:'sp'
+        }
+      }]}
+  	)})
+  	.fail(function() {
+    	console.log( "error" );
+  	})
+  	.always(function() {
+    	console.log( "complete" );
+  	});
+    });
